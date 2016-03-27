290241c4c02eb300416616fcf58002007117cf5b
JohnWhisker <nguyen.dk0952@sinhvien.hoasen.edu.vn>
2016-03-27 20:03:10 +0700

    BASIC

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetArrayAdapter.java
@@ -1,15 +1,53 @@
 package com.codepath.apps.restclienttemplate;
 
-import android.content.Context;
-import android.widget.ArrayAdapter;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.List;
+import org.w3c.dom.Text;
 
 /**
  * Created by johnw on 3/27/2016.
  */
-public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
-    public TweetArrayAdapter (Context context, List<Tweet> tweets){
-        super(context,android.R.layout.simple_list_item_1,tweets);
+public class TweetArrayAdapter extends RecyclerView.Adapter<TweetArrayAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView tvName,tvDate,tvBodyText;
+        private ImageView ivPhoto,ivProfilePicture;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            tvName = (TextView) view.findViewById(R.id.tvName);
+            tvDate = (TextView) view.findViewById(R.id.tvDate);
+            tvBodyText = (TextView) view.findViewById(R.id.tvBodyText);
+            ivPhoto = (ImageView) view.findViewById(R.id.ivPhoto);
+            ivProfilePicture = (ImageView) view.findViewById(R.id.ivProfilePicture);
+
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    @Override
+    public Tweet onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Tweet holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
